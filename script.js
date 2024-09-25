a0a255f7ee6bfcb723dc3a0beac55b223a7c4b9a
unknown <leandroezequielpedercini@gmail.com>
2024-09-25 03:04:16 -0300

    Modificancionés al sitio wey

--- script.js
+++ script.js
@@ -7,15 +7,58 @@ cabezal.innerHTML =  `<div class="div-header"> <img class="header-img1" src="/im
 let titulo = document.getElementById("titulo")
 titulo.innerHTML = `<img class="titulo" src="/img/2-titulo/titulo.jpg" alt="">`
 
+
+/* main */
+let main = document.getElementById("main")
+main.innerHTML = `<div class="main-tarjetas " >
+
+            <div class="primera-tarjeta">
+            <div class="card" style="width: rem;">
+                <img class="img-uno" src="/img/3-tarjetas/foto1.jpg
+                " class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Pelicula 1</h5>
+                  <p class="card-text"></p>
+                <a id="btn-uno" href="#" class="btn btn-primary">Ver el trailer</a>
+          
+                    </div>
+              </div>
+        </div>
+
+        <div class="segunda-tarjeta">
+            <div class="card" style="width: 10.7rem;">
+                <img class="img-dos" src="/img/3-tarjetas/foto2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Pelicula 2</h5>
+                  <p class="card-text"></p>
+                  <a id="btn-dos" href="#" class="btn btn-primary">Ver el trailer</a>
+                </div>
+              </div>
+        </div>
+
+        <div class="primera-tarjeta">
+            <div class="card" style="width: 11.2rem;">
+                <img class="img-tres" src="/img/3-tarjetas/foto3.jpg
+                " class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Pelicula 3</h5>
+                  <p class="card-text"></p>
+                  <a id="btn-tres" href="#" class="btn btn-primary">Ver el trailer</a>
+                </div>`
+
+
 let botonUno = document.getElementById("btn-uno").onclick = function() {
-let traillerUno = document.getElementById("trailer-uno").play() 
+document.getElementById("trailer-uno").play()
+
 }
 
+
+
 let botonDos = document.getElementById("btn-dos").onclick = function() {
-let traillerDos = document.getElementById("trailer-dos").play()
+  document.getElementById("trailer-dos").play()
 }
 
 let botonTres = document.getElementById("btn-tres").onclick = function() {
-let trailerTres = document.getElementById("trailer-tres").play()
+  document.getElementById("trailer-tres").play()
 }
 
