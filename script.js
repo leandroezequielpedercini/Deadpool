39451ce734b7259cfd484a98dbd2be7172a2e743
Leandro Ezequiel Pedercini <leandroezequielpedercini@gmail.com>
2024-12-16 22:46:36 -0300

    ..

---
+++ script.js
@@ -0,0 +1,25 @@
+const header = document.getElementById("header")
+header.innerHTML = `<div><img src="index/img/1-header/foto1.jpg" alt="MARVEL STUDIOS" class="titulo" ></div>
+                    
+                    <div><img src="index/img/2-titulo/titulo.jpg" alt="DEADPOOL" class="subtitulo" ></div>`
+
+
+
+const seccion = document.getElementById("seccion")
+seccion.innerHTML = `<div class="cartas">
+    
+    
+    </div>`
+
+
+    //main//
+
+
+
+
+
+
+
+
+const footer = document.getElementById("footer")
+footer.innerHTML = ` <div class="footer"> <p class="parrafoFooter" >Solo en cines..</p> <div/> `
