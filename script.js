f05802074c763c446a2f6a7581a4a9cde14411b0
Leandro Ezequiel Pedercini <leandroezequielpedercini@gmail.com>
2024-12-19 18:04:19 -0300

    ..

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ header.innerHTML = `<div><img src="index/img/1-header/foto1.jpg" alt="MARVEL STU
 
 
 const seccion = document.getElementById("seccion")
-seccion.innerHTML = `<div class="cartas">
+seccion.innerHTML = `<div class="cartas1">
     
+<video class="video1" src="index/Video/traillerUno.mp4"</video>
+<video class="video2" src="index/Video/traillerDos.mp4"></video>
+<video class="video3" src="index/Video/traillerTres.mp4"></video>
+    </div>`
+
+    //main//
+
+const main = document.getElementById("main")
+main.innerHTML = `<div class="cartas2">
+
 <img src="index/img/3-tarjetas/foto1.jpeg" alt="Deadpool 1">
 <img src="index/img/3-tarjetas/foto2.jpeg" alt="Deadpool 2">
 <img src="index/img/3-tarjetas/foto3.jpeg" alt="Deadpool 3">    
     </div>`
-
-
-    //main//
-
-
-
-
-
-
-
 
 const footer = document.getElementById("footer")
 footer.innerHTML = ` <div class="footer"> <p class="parrafoFooter" >Solo en cines..</p> <div/> `
