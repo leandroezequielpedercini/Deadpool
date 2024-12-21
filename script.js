9f1660d15f573e667d80c279d719ea0c42ce5348
Leandro Ezequiel Pedercini <leandroezequielpedercini@gmail.com>
2024-12-21 05:31:32 -0300

    ..

--- script.js
+++ script.js
@@ -40,41 +40,82 @@ seccion.innerHTML = `<div class="cartas1">
     //main//
 
 const main = document.getElementById("main")
-main.innerHTML = `
-<div class="cartas2">
+main.innerHTML = ` <div class="cartas2">
 
-    <div>
-    <img id="carrito1" class="movie1" src="index/img/3-tarjetas/foto1.jpeg" alt="Deadpool 1">
-   </div>
-   
-   <div>
-   <input class="carroUno" type="button" value="Añadir al carrito" onclick="carro1()">
-    </div>
+<div>
 
-    <div>
-    <img id="carrito2" class="movie2" src="index/img/3-tarjetas/foto2.jpeg" alt="Deadpool 2">
-    </div>
+<img id="carrito1" class="movie1" src="index/img/3-tarjetas/foto1.jpeg" alt="Deadpool 1">
+
+<input class="carroUno" type="button" value="Añadir al carrito" onclick="btrcarro1()">
+
+<p id="pelicula1" class="peliculaUno" > Pelicula: </p>
+
+<p">Deadpool (1)</p>
+
+    <p >Precio:</p>
+    <p class="preciouno" id="precio1"></p>
     
-    <div>
-    <input class="carroDos"  type="button" value="Añadir al carrito" onclick="carro2()" >
-    </div>
+</div>
 
-    <div>
-    <img id="carrito1"  class="movie3" src="index/img/3-tarjetas/foto3.jpeg" alt="Deadpool 3">    
-    <div>
-   
-    <div>
-    <input class="carroTres"" type="button" value="Añadir al carrito" onclick="carro3()" >  
-    </div>
+
+
+
+<div>
+<img id="carrito2" class="movie2" src="index/img/3-tarjetas/foto2.jpeg" alt="Deadpool 2">
+<input class="carroDos"  type="button" value="Añadir al carrito" onclick="carro2()" >
+
+<p id="pelicula2" class="peliculaDos" > Pelicula: </p>
+
+<p id="precio2">Precio:</p>
+</div>
+
+
+<div>
+
+<img id="carrito1"  class="movie3" src="index/img/3-tarjetas/foto3.jpeg" alt="Deadpool 3">    
+
+<input class="carroTres" type="button" value="Añadir al carrito" onclick="carro3()" >  
+
+<p id="pelicula3" class="peliculaTres" > Pelicula: </p>
+
+<p id="precio3">Precio:</p>
+
+<div>
+
+</div>`
+
+
     
-    </div>`
-
-
     const peliculas = [
         {
+            preciouno: "$2.000",
+            preciodos: "$2.500",
+            preciotres: "$5.000"
         }
-    ]
+    ]      
 
+           
+        
+
+
+
+    const contador = 0
+
+    const  carrito =document.getElementById("carrito") 
+    carrito.innerHTML = [""];
+
+    function btrcarro1(){
+        
+    
+        for(i=0; i < peliculas.length; i++){ 
+
+            const peli1 = document.getElementById("precio1")
+            peli1.innerHTML +=  (peliculas[i].preciouno) 
+    }
+    carrito.appendChild(peli1);
+
+    }
+    
 
 
 const footer = document.getElementById("footer")
